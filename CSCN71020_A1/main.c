47a1db3087406efe1ad51103bf3ff0249c60660b
gignatov6034 <gignatov6034@conestogac.on.ca>
2021-09-30 15:46:55 -0400

    working_subtract

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -20,6 +21,8 @@ void main() {
 	{
 	case 1:
 		add();
+	case 2:
+		subtract();
 	}
 
 }
@@ -44,4 +47,22 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+	double num1, num2, result;
+
+	printf("Enter the first value: ");
+	scanf_s("%lf", &num1);
+
+	printf("Enter the second value: ");
+	scanf_s("%lf", &num2);
+
+	if (num1 != 0)
+	{
+		result = num2 / num1;
+		printf("%lf / %lf = %lf\n", num2, num1, result);
+	}
+	else
+		printf("Cannot be divided by zero\n");
 }
